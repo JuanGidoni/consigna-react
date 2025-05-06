48059907acd87d2a7bbc35d528886b2b4c270b40
su4nsi <miguelperatorres@gmail.com>
2025-05-06 19:33:34 +0200

    feat: add name to url

--- miguel pokeapi/vite.config.js
+++ miguel pokeapi/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/pokemigapi/",
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.js",
+    include: ["**/*.test.js", "**/*.test.jsx"], 
+  },
+});
