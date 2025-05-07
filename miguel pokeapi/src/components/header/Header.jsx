59be25f696f0666e076234784af166503c149200
miguel.pera <miguel.pera@estudiantat.upc.edu>
2025-05-07 14:31:51 +0200

    feat: add header to layout

---
+++ miguel pokeapi/src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import logoImgHeader from "../../assets/logo.png";
+import "./Header.css";
+
+export function Header() {
+  return (
+    <header className="main-header">
+      <div className="header-container">
+        <img
+          src={logoImgHeader}
+          alt="the logo of Pokemigapi"
+          className="logo"
+        />
+        <h1 className="title">Pokemigapi</h1>
+      </div>
+
+    </header>
+  );
+}
+
+export default Header;
